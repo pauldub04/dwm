3505313e69a578d777a598bf4c22733b608942bf
pauldub04 <paul.dubnyakov@gmail.com>
2024-07-23 03:15:36 +0300

    configure activetagindicatorbar

--- dwm.c
+++ dwm.c
@@ -700,7 +700,7 @@ drawbar(Monitor *m)
 {
 	int x, w, tw = 0;
 	int boxs = drw->fonts->h / 9;
-	int boxw = drw->fonts->h / 6 + 2;
+	int boxw = drw->fonts->h / 6;
 	unsigned int i, occ = 0, urg = 0;
 	Client *c;
 
